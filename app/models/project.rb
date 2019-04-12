68ed04b3afbcff6d0f6a0253731bcd0861146a6e
Sharmanidhi <nidhi.sharma2015@sait.ac.in>
2019-04-12 14:57:27 +0530

    initial commit

---
+++ app/models/project.rb
@@ -0,0 +1,4 @@
+class Project < ApplicationRecord
+  has_many :tasks, inverse_of: :project
+  accepts_nested_attributes_for :tasks , reject_if: proc { |attributes| attributes[:name].blank? } ,allow_destroy: true
+end
